ce5df5244191188e7dcbd5a8f8c69a66e8be3c68
Berkcan Gümüşışık <75336900+berkcangumusisik@users.noreply.github.com>
2022-11-29 10:30:27 +0300

    Week 5 finished

---
+++ Week 5/Problem Sets 5/test_bank/bank.py
@@ -0,0 +1,17 @@
+def main():
+    text = input("Greeting: ")
+    greeting = value(text)
+    print(greeting)
+
+def value(greeting):
+    greeting = greeting.lower().strip()
+
+    if greeting.startswith("hello"):
+        return 0
+    elif greeting.startswith("h"):
+        return 20
+    else:
+        return 100
+
+if __name__ == "__main__":
+    main()
